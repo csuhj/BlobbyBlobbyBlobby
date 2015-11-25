5b56ea133839a535a687a9868816aa9b6146eb21
csuhj <csuhj@users.noreply.github.com>
2015-11-25 11:30:59 +0000

    Added websocket support to the server and client so that the client sends the centre of the viewport to the server and the server echos it back.  Currently this is independent of how the game actually works, but this is just to show that two-way communication can take place easily enough

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+
 var app = express();
 app.use(bodyParser.json());
 
 app.use("/", require("./static"));
 
-app.listen(8080, function() {
+var server = http.createServer(app);
+server.listen(8080, function() {
     console.log("server listening on", 8080);
+});
+
+var wss = new WebSocketServer({server: server});
+
+wss.on("connection", function(ws) {
+    console.log("websocket connection open");
+
+    ws.on("close", function() {
+        console.log("websocket connection close");
+    });
+
+    ws.on('message', function incoming(message) {
+        ws.send(JSON.stringify(message), function() {  })
+    });
 });
