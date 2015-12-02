65183ce4bf495d61901977dbd0031ab534cb1486
csuhj <csuhj@users.noreply.github.com>
2015-12-02 22:35:30 +0000

    Updated version of python Blobby client

--- static/Blobby.py
+++ static/Blobby.py
@@ -19,52 +19,44 @@ WHITE      = (255, 255, 255)
 BLACK      = (  0,   0,   0)
 
 @asyncio.coroutine
-def main():
-#    websocket = yield from websockets.connect('wss://polar-forest-9270.herokuapp.com/')
-#    websocket = yield from websockets.connect('ws://polar-forest-9270.herokuapp.com/')
-#    websocket = yield from websockets.connect('ws://blobby.larj.uk/')
-    websocket = yield from websockets.connect('ws://localhost:8080/')
+def readGameState(websocket, gameState):
+    while True:
+        gameStateDeltaString = yield from websocket.recv()
+        print(gameStateDeltaString)
 
+        gameStateDelta = json.loads(gameStateDeltaString)
+        for newFood in gameStateDelta['foodDelta']['newFood']:
+            gameState['food'][newFood['id']] = newFood
+
+        for eatenFood in gameStateDelta['foodDelta']['eatenFood']:
+            gameState['food'].pop(eatenFood, None)
+
+        gameState['players'] = gameStateDelta['players']
+        gameState['me'] = gameStateDelta['me']
+
+@asyncio.coroutine
+def main():
+    websocket = yield from websockets.connect('wss://polar-forest-9270.herokuapp.com/')
 
     global FPSCLOCK, DISPLAYSURF
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 
     gameState = { 'food': {}, 'players': [], 'me': None }
-    mousex = 0
-    mousey = 0
     pygame.display.set_caption('Memory Game')
 
     gameStateDelta = []
-#    asyncio.async(readGameState(fullState))
+    asyncio.async(readGameState(websocket, gameState))
+
     while True:
         pygame.event.pump() # advance pygame event loop
-        
+
         DISPLAYSURF.fill(WHITE)
-        
-        gameStateDeltaString = yield from websocket.recv()
 
-        gameStateDelta = json.loads(gameStateDeltaString)
-        for newFood in gameStateDelta['foodDelta']['newFood']:
-            gameState['food'][newFood['id']] = newFood
-        
-        for eatenFood in gameStateDelta['foodDelta']['eatenFood']:
-            gameState['food'].pop(eatenFood, None)
-        
-        gameState['players'] = gameStateDelta['players']
-        gameState['me'] = gameStateDelta['me']
-        
-        for k,v in gameState['food'].items():
-            drawBlobby(gameState['me'], v)
+        drawBlobbies(gameState)
 
-        for player in gameState['players']:
-            drawBlobby(gameState['me'], player)
-
-        drawBlobby(gameState['me'], gameState['me'])
-
-        
         pygame.display.update()
-        
+
         for event in pygame.event.get(): # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 yield from websocket.close()
@@ -75,17 +67,28 @@ def main():
                 offsetx = mousex - (WINDOWWIDTH / 2)
                 offsety = mousey - (WINDOWHEIGHT / 2)
                 yield from websocket.send('{\"x\":' + str(offsetx)+ ', \"y\": ' + str(offsety) + '}')
-        
+
         yield from asyncio.sleep(0.016) # advance asyncio loop
+
+def drawBlobbies(gameState):
+    if (gameState['me'] != None):
+        for k,v in gameState['food'].items():
+            drawBlobby(gameState['me'], v)
+
+        for player in gameState['players']:
+            drawBlobby(gameState['me'], player)
+
+        drawBlobby(gameState['me'], gameState['me'])
 
 def drawBlobby(me, blobby):
     viewPortLeft = me['x'] - CENTREX;
     viewPortTop = me['y'] - CENTREY;
-    
+
     offsetX = int(blobby['x'] - viewPortLeft);
     offsetY = int(blobby['y'] - viewPortTop);
-    
+
     pygame.draw.circle(DISPLAYSURF, getColour(blobby['colour']), (offsetX, offsetY), int(blobby['size']))
+    pygame.draw.circle(DISPLAYSURF, BLACK, (offsetX, offsetY), int(blobby['size']), 1)
 
 def getColour(colourName):
     if colourName == 'yellow':
