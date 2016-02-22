57fbc141d3c6b08bfc4db1bc41ea77a833aa6049
csuhj <csuhj@users.noreply.github.com>
2016-02-22 21:13:26 +0000

    made it possible to connect to Mongo lab on Heroku or locally

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var url = process.env.MONGOLAB_URI || "mongodb://localhost/blobby";
 
 mongoose.connect(url, function() {
-    console.log("mongodb connected");
+    console.log("mongodb connected to "+url);
 });
 
 module.exports = mongoose;
